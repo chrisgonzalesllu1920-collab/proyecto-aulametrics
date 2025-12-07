4cf4ebaccae65a6c837754121443d6f31bf87746
chrisgonzalesllu1920-collab <chrisgonzalesllu1920@gmail.com>
2025-12-06 21:53:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ import modules.evaluacion as evaluacion
 import modules.asistente as asistente
 import modules.ui_components as ui
 import modules.database as db
+from modules import gamificacion
 from supabase import create_client, Client
 # --- FUNCIÓN PARA CARGAR ROBOTS (LOTTIE) ---
 def cargar_lottie(filepath):
@@ -1406,6 +1407,7 @@ if not st.session_state.logged_in:
     login_page()
 else:
     home_page()
+
 
 
 
