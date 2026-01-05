abbd8c07a8abc361b0d78db3507ec803ac8fa3c8
chrisgonzalesllu1920-collab <chrisgonzalesllu1920@gmail.com>
2026-01-04 23:15:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -458,7 +458,7 @@ def login_page():
     <style>
         /* 1. FONDO DEGRADADO */
         [data-testid="stAppViewContainer"] {
-            background: linear-gradient(135deg, #FF5E9E 0%, #A0E85A 100%);
+            background: linear-gradient(135deg, #2c3e50 0%, #1a252f 100%);
             background-size: cover;
             background-attachment: fixed;
         }
@@ -1200,6 +1200,7 @@ if not st.session_state.logged_in:
     login_page()
 else:
     home_page()
+
 
 
 
